6bfbf9e4bddcba9a1e97fc89da9426db7caeb14b
01036_victor <01036_victor@MacBook-Pro-de-DATUM.local>
2024-12-12 14:37:40 -0300

    feat: first-exercise

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+name = 'Victor'
+last_name = 'Rodrigues Novais'
+age = 28
+burn_date = 2024 - age
+major_age = age >= 18
+height = 1.75
+
+print('Name', name)
+print('Last Name', last_name)
+print('Age', age)
+print('Burn Date', burn_date)
+print('Major Age', major_age)
+print('Height', height)
