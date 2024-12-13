8e095e8098e2792cbedb254f694dfa8e453f1a45
01036_victor <01036_victor@mac.home>
2024-12-13 15:16:36 -0300

    feat: generate cpf

--- generate_cpf.py
+++ generate_cpf.py
@@ -1,10 +1,10 @@
-import re
+import random
 
-cpf_number = input('Digite seu CPF: ')
+for _ in range(100):
+  first_nine_numbers = ''
+  for _ in range(9):
+    first_nine_numbers += str(random.randint(0, 9))
 
-try:
-  cpf_number = re.sub(r'[^0-9]', '',cpf_number)
-  first_nine_numbers = cpf_number[:9]
   first_digit_result = 0
 
   for index, number in enumerate(list(range(10, 1, -1))):
@@ -12,7 +12,6 @@ try:
 
   first_digit = (first_digit_result * 10) % 11
   first_digit = first_digit if first_digit <= 9 else 0
-  print(first_digit)
 
   second_ten_numbers = first_nine_numbers + str(first_digit)
   second_digit_result = 0
@@ -22,8 +21,6 @@ try:
 
   second_digit = (second_digit_result * 10) % 11
   second_digit = second_digit if second_digit <= 9 else 0
-  print(second_digit)
 
-
-except ValueError:
-  print('Você não digitou um número inteiro.')
+  generated_cpf = f'{first_nine_numbers}{first_digit}{second_digit}'
+  print(generated_cpf)
